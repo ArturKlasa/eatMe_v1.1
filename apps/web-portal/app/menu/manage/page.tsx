c1a7e3fe82234264e37f756c8c94f1002756b94f
ArturKlasa <klasaartur@gmail.com>
2026-06-18 23:13:43 -0500

    deleting old web-portal

--- apps/web-portal/app/menu/manage/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function MenuManagePage() {
-  // Redirect to existing menu page for now
-  if (typeof window !== 'undefined') {
-    window.location.href = '/onboard/menu';
-  }
-  return null;
-}
